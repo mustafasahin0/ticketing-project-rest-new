2565af40e4f9959e1349b407bd4479ea4f057448
mustafasahin0 <sahinmustafaenes4@gmail.com>
2024-01-11 20:33:05 -0700

    Mock MVC

--- src/test/java/org/example/controller/ProjectControllerTest.java
+++ src/test/java/org/example/controller/ProjectControllerTest.java
@@ -42,7 +42,7 @@ class ProjectControllerTest {
     @BeforeAll
     static void setUp() {
 
-        token = "Bearer ";
+        token = "Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICIwbV9taDF0cU9fcm02N0E2WEVhSEpTZnFpOHBXcWdudzVTVUE2d0lJWS1FIn0.eyJleHAiOjE3MDUwMjk1MjcsImlhdCI6MTcwNTAyOTIyNywianRpIjoiYTE1NzRmYmEtOTJmMS00NDM5LWEyMzgtYWU3MWNmZGRmNmUzIiwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo4MDgwL2F1dGgvcmVhbG1zL2V4YW1wbGUtZGV2IiwiYXVkIjoiYWNjb3VudCIsInN1YiI6IjhkNzMwNjEyLWI1YTYtNDAyOS05MTEyLThjZDg5NGVjNTYwZSIsInR5cCI6IkJlYXJlciIsImF6cCI6InRpY2tldGluZy1hcHAiLCJzZXNzaW9uX3N0YXRlIjoiYjUxYzUyYTAtYWE5YS00NGY5LWJlMjEtOGUxYWU4NWNmYmIyIiwiYWNyIjoiMSIsImFsbG93ZWQtb3JpZ2lucyI6WyJodHRwOi8vbG9jYWxob3N0OjgwODEiXSwicmVhbG1fYWNjZXNzIjp7InJvbGVzIjpbIm9mZmxpbmVfYWNjZXNzIiwidW1hX2F1dGhvcml6YXRpb24iLCJkZWZhdWx0LXJvbGVzLWV4YW1wbGUtZGV2Il19LCJyZXNvdXJjZV9hY2Nlc3MiOnsidGlja2V0aW5nLWFwcCI6eyJyb2xlcyI6WyJNYW5hZ2VyIl19LCJhY2NvdW50Ijp7InJvbGVzIjpbIm1hbmFnZS1hY2NvdW50IiwibWFuYWdlLWFjY291bnQtbGlua3MiLCJ2aWV3LXByb2ZpbGUiXX19LCJzY29wZSI6Im9wZW5pZCBlbWFpbCBwcm9maWxlIiwic2lkIjoiYjUxYzUyYTAtYWE5YS00NGY5LWJlMjEtOGUxYWU4NWNmYmIyIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsInByZWZlcnJlZF91c2VybmFtZSI6Im1pa2UifQ.m-4uWrYJGwEP14hnoYZ3EbkRKFx8fwWWA9vpebNjx0GodiJZGJVn61Nu7m7JOSEwomwxscJ50WB8oFeiVOf_QLHV1lHyVOh4QaHJ4iAtUbnnnzPlJcS3Wcm7TRDpFx_czVZ81kt08nSEFIPUpLeaPJoZY2tZVlxymoGBO7vqNkisYVUhTzSNgOYNoPlYu4Waw4zQeE_HemtvZUHi1Kw7bvrkCQjLl-5f7-aawbQU1udKLb7N-clY8Kq3FYmzoxOmcbrOr7xhrYOphjXD0KSG6woWrUJulUd-tVaLJQ1zFbLSO2i2pqH7rG9aokLu7MF7an00NOj3eNSmTuhug_iGEQ";
 
         userDTO = UserDTO.builder()
                 .id(2L)
@@ -104,16 +104,13 @@ class ProjectControllerTest {
     @Test
     public void givenToken_updateProject() throws Exception {
 
-        projectDTO.setProjectName("Api-cydeo");
-
-        mvc.perform(MockMvcRequestBuilders
-                        .put("/api/v1/project")
+        projectDTO.setProjectName("Api-mike");
+        mvc.perform(MockMvcRequestBuilders.put("/api/v1/project")
                         .header("Authorization", token)
                         .content(toJsonString(projectDTO))
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("message").value("Project is successfully updated"));
-
     }
 
     @Test
